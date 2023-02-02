7bea144920a08465cd41014a811b0d29bdde36e6
Malik <willis.malik.r@gmail.com>
2023-02-01 22:51:14 -0600

    Hello world

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,14 @@
+public class HelloWorld {
+    public static void main(String[] args){
+        //System.out.print
+        // Similar to a file path dot being like (/) forward slash
+        // Could be read as, "In our System find the output in our output print what ever is in the quotations
+        System.out.print("Hello World");
+        System.out.print(" & Malik!");
+        System.out.print("Cat\n");
+        System.out.print("Dog\n");
+        System.out.print("Rabbit\n");
+
+        // main method runs all statements above then terminates before reaches last bracket in main method
+    }
+}
