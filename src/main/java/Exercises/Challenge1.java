e715ec98a21dbb4850d4b6b6a6ab8a19d436405b
Malik <willis.malik.r@gmail.com>
2023-02-02 00:22:53 -0600

    Exercise 3 complete

--- src/main/java/Exercises/Challenge1.java
+++ src/main/java/Exercises/Challenge1.java
@@ -2,6 +2,8 @@ package Exercises;
 
 public class Challenge1 {
     public static void main(String[] args){
+
+        //      Exercise 1 & 2
         System.out.println("SHOPPING LIST");
         System.out.println("\t- Milk");
         System.out.println("\t\t> Semi-skimmed");
@@ -17,6 +19,12 @@ public class Challenge1 {
         System.out.print("\t- Eggs\n");
         System.out.print("\t\t> Free-range\n");
         System.out.print("\t- Bread\n");
+
+        // Exercise 3
+        System.out.println("The");
+        System.out.println("\tCake");
+        System.out.println(" Is\t\tA");
+        System.out.println("\tLie.");
     }
 
 
