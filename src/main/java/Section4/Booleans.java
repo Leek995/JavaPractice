1be071890d0e783ecd36203f475881546dcf4fb5
Malik <willis.malik.r@gmail.com>
2023-02-02 01:39:11 -0600

    char, boolean, and String data types.

---
+++ src/main/java/Section4/Booleans.java
@@ -0,0 +1,23 @@
+package Section4;
+
+public class Booleans {
+    public static void main(String[] args){
+        int a = 27;
+        double b = 14.27;
+        boolean c = false;
+        // char is based on uni code therefore 77 equals M character
+        // char range is from 0 - 65XXX, where any number equals character or symbol
+        // chars have to be in single quotes not double quotes
+        char d = 62311;
+        char e = 'M';
+        char f = '\u0041';
+        // String is not a data type!! It is a reference type that refers to String Obj.
+        // Sting has to be in double quotes.
+        String myName = "Malik";
+        System.out.print("Abso" + "lutely\n");
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+    }
+}
