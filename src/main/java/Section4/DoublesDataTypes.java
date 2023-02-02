46e939d5c65574a6acd866dc438f76c04fa86145
Malik <willis.malik.r@gmail.com>
2023-02-02 00:54:30 -0600

    doubles and floats

---
+++ src/main/java/Section4/DoublesDataTypes.java
@@ -0,0 +1,14 @@
+package Section4;
+
+public class DoublesDataTypes {
+    public static void main(String[] args){
+        // int data type is a primitive data type
+        // 8 data types in java
+        // byte, short, int, long, float, double, boolean, char
+        // byte, short, int, long are integer types bc they can potentially hold each other
+        int guitar = 10;
+        double x  = 3;
+        System.out.println(guitar - x);
+
+    }
+}
