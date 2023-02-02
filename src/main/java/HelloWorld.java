a741ab7eca9a355ea05cdb42e934c31ebadc4f3b
Malik <willis.malik.r@gmail.com>
2023-02-01 23:01:56 -0600

    escape sequences

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -4,10 +4,16 @@ public class HelloWorld {
         // Similar to a file path dot being like (/) forward slash
         // Could be read as, "In our System find the output in our output print what ever is in the quotations
         System.out.print("Hello World");
-        System.out.print(" & Malik!");
-        System.out.print("Cat\n");
-        System.out.print("Dog\n");
-        System.out.print("Rabbit\n");
+        System.out.print(" & Malik!\n");
+        // println doesn't add new line to beginning only the end
+        System.out.println("Cat");
+        System.out.println("Dog\n");
+        System.out.print("Rab\n\tbit\n");
+        System.out.println("Ra\bbbi\b\bt");
+        // escape characters backslash comes first then character
+        System.out.println("The dog said \"Woof\", the rolled around!");
+        // double backlashes gives one backlash
+        System.out.println("C:\\Users\\bob");
 
         // main method runs all statements above then terminates before reaches last bracket in main method
     }
