a3f4630380ff20c6075e4f10493d8c9402308d9c
Malik <willis.malik.r@gmail.com>
2023-02-03 14:25:49 -0600

    scanner indepth refresher

---
+++ src/main/java/Section5/ScannerMethod.java
@@ -0,0 +1,11 @@
+package Section5;
+
+import java.util.Scanner;
+
+public class ScannerMethod {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String b = sc.next();
+        System.out.println(b);
+    }
+}
