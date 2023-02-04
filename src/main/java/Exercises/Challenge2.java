e0deab61feff24718fb35a1609fefe9456a4af57
Malik <willis.malik.r@gmail.com>
2023-02-03 18:44:21 -0600

    Challenge 2 exercise complete

---
+++ src/main/java/Exercises/Challenge2.java
@@ -0,0 +1,16 @@
+package Exercises;
+
+import java.util.Scanner;
+
+public class Challenge2 {
+    public static void main(String[] args){
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("What is your age?");
+        int userInputAge = scanner.nextInt();
+        System.out.format("You've lived %d, and next year if the Lord is willing, you'll be %d.\n" ,userInputAge, userInputAge + 1);
+        System.out.println("What is your name?");
+        String firstName = scanner.next();
+        System.out.format("You have %d characters in your name.",firstName.length());
+    }
+}
