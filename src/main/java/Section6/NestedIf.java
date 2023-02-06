c4f687ac1c191f65f094ccbb0c83be53f6f196e7
Malik <willis.malik.r@gmail.com>
2023-02-06 00:53:32 -0600

    improved nested if

--- src/main/java/Section6/NestedIf.java
+++ src/main/java/Section6/NestedIf.java
@@ -15,13 +15,16 @@ public class NestedIf {
 
 
         if(age >= 18){
-            System.out.print("Do you have a VIP Pass yes/no:");
-            vipPassReply = sc.next();
-
-            if(vipPassReply.equals("yes") || age >= 70){
+            if(age >= 70){
                 System.out.println("Thanks, go in.");
             }else{
-                System.out.println("Sorry, get a pass");
+                System.out.print("Do you have a VIP Pass yes/no:");
+                vipPassReply = sc.next();
+                if(vipPassReply.equals("yes")){
+                    System.out.println("Thanks, go in.");
+                }else{
+                    System.out.println("Sorry, get a pass");
+                }
             }
         }else {
             System.out.println("Scram get out of here!");
