6ab2a4b0d5ef3ba6829d4e4e753f3c61af83aa03
Malik <willis.malik.r@gmail.com>
2023-02-07 01:17:23 -0600

    modulo operator

---
+++ src/main/java/Section7/Modulo.java
@@ -0,0 +1,10 @@
+package Section7;
+
+public class Modulo {
+    public static void main(String[] args){
+        System.out.println(5 % 15);
+        System.out.println(15 % 5);
+        System.out.println(15 % 2 == 0);
+        System.out.println(15 % 1 == 1);
+    }
+}
