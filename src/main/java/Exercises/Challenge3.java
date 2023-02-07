55f5208af4c89e19c5e42ebb2410931ad7c08c98
Malik <willis.malik.r@gmail.com>
2023-02-06 22:00:35 -0600

    difference of two included, modified exercise one.

--- src/main/java/Exercises/Challenge3.java
+++ src/main/java/Exercises/Challenge3.java
@@ -7,6 +7,7 @@ public class Challenge3 {
     public static void main(String[] args){
         int x;
         int y;
+        int differenceXY;
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Enter in two numbers.");
@@ -15,14 +16,16 @@ public class Challenge3 {
         x = scanner.nextInt();
 
         System.out.print("Enter number: ");
-        y= scanner.nextInt();
+        y = scanner.nextInt();
+
+        differenceXY = x - y;
 
         if(x > y){
-            System.out.format("%d is greater than %d", x, y);
+            System.out.format("%d is greater than %d.\nThe difference is %d.", x, y, differenceXY);
         } else if (x < y) {
-            System.out.format("%d is smaller than %d", x, y);
+            System.out.format("%d is smaller than %d.\nThe difference is %d.", x, y, differenceXY);
         } else if(x == y){
-            System.out.format("%d is equal to %d", x, y);
+            System.out.format("%d is equal to %d.\nThe difference is %d.", x, y, differenceXY);
         }
 
     }
