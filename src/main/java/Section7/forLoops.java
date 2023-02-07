fbade2eb27d6b6000a2d9fb7bc1ee9ea33b2ad3c
Malik <willis.malik.r@gmail.com>
2023-02-06 23:43:03 -0600

    for loops

---
+++ src/main/java/Section7/forLoops.java
@@ -0,0 +1,13 @@
+package Section7;
+
+public class forLoops {
+    public static void main(String[] args){
+        for (int i = 0; i < 10; ++i){
+            System.out.println(i);
+        }
+        System.out.println();
+        for (int i = 1250; i >= 480; i--){
+            System.out.println(i);
+        }
+    }
+}
